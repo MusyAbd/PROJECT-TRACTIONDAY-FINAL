92920d2c610d0c96332440580f295a4b972c21d6
MusyAbd <musyaffaabdullah@gmail.com>
2026-06-25 14:36:25 +0700

    vbn

--- api/index.php
+++ api/index.php
@@ -1,13 +1,8 @@
 <?php
 session_start();
 
-// Redirect to login if user is not authenticated
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+
 	header('Location: /api/dashboard_with_qr_copy.php');
-	exit;
-}
-else {
-	header('Location: /api/dashboard_with_qr_copy.php');
-}
+
 
 include 'qr_secure.php';
