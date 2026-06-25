dd808f0a9a88ce03b93d258607843fa0b19d8638
MusyAbd <musyaffaabdullah@gmail.com>
2026-06-25 14:28:55 +0700

    asdasdasdasdasccccc

--- api/index.php
+++ api/index.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
 
+// Redirect to login if user is not authenticated
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+	header('Location: /dashboard_with_qr_copy.php');
+	exit;
+}
+else {
+	header('Location: /dashboard_with_qr_copy.php');
+}
+
 include 'qr_secure.php';
-?>
