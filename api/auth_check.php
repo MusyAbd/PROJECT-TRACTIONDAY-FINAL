c90ac854e026f7f9d2038c6c457e5ac22c48fb39
MusyAbd <musyaffaabdullah@gmail.com>
2026-06-25 14:18:44 +0700

    MASSIVE-CHANGE

--- api/auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Cek apakah cookie loggedin belum ada atau nilainya bukan true
-if (!isset($_COOKIE["loggedin"]) || $_COOKIE["loggedin"] !== "true") {
-    // Jika belum login, tendang ke halaman login
-    header("Location: /login.php");
-    exit();
-}
-?>
--- api/dashboard_with_qr_copy.php
+++ api/dashboard_with_qr_copy.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/auth_check.php';
+require_once __DIR__ . 'config/koneksi.php';
 ?>
 <!DOCTYPE html>
 <html lang="id">
