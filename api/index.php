d03842b9cd1ceac00de399ecb72edb755b7eee3b
MusyAbd <musyaffaabdullah@gmail.com>
2026-06-25 14:04:58 +0700

    reset

--- api/index.php
+++ api/index.php
@@ -2,8 +2,9 @@
 session_start();
 
 // Redirect to login if user is not authenticated
-
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 	header('Location: /api/dashboard_with_qr_copy.php');
-
+	exit;
+}
 
 include 'qr_secure.php';
