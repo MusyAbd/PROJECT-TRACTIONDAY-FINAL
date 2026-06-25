30581843ee2e4c5fe30c1f8714b862d02053d462
MusyAbd <musyaffaabdullah@gmail.com>
2026-06-25 10:24:20 +0700

    last pls

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@ session_start();
 
 // Redirect to login if user is not authenticated
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-	header('Location: api/login.php');
+	header('Location: /login.php');
 	exit;
 }
 
